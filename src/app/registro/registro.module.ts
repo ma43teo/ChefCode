b3c52dcfc38872938be6be227c2de8db3ad9f887
Steyyyyy27 <163907099+Steyyyyy27@users.noreply.github.com>
2024-07-31 23:54:35 -0600

    cmb registro1

--- src/app/registro/registro.module.ts
+++ src/app/registro/registro.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
 
 import { RegistroPageRoutingModule } from './registro-routing.module';
-
 import { RegistroPage } from './registro.page';
 
 @NgModule({
--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -6,22 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage {
-
-  customCounterFormatter(inputLength: number, maxLength: number): string {
-    return `${inputLength} / ${maxLength}`;
-  }
-
-  phoneNumberError: boolean = false;
-
-  validatePhoneNumber(event: any) {
-    const phoneNumberPattern = /^[0-9]{10}$/;
-    this.phoneNumberError = !phoneNumberPattern.test(event.target.value);
-  }
-
-  register() {
-    // Aquí puedes implementar la lógica para registrar al usuario
-    console.log('Registrando usuario...');
-    // Puedes agregar aquí la lógica para enviar los datos del formulario al servidor
-  }
-
+  constructor() {}
 }
