708fd15c84d0b8e56c88f4bb9028fdcf15369a70
Steyyyyy27 <163907099+Steyyyyy27@users.noreply.github.com>
2024-08-30 23:32:43 -0600

    platos-principales_cmb1

--- src/app/platos-principales/platos-principales.page.ts
+++ src/app/platos-principales/platos-principales.page.ts
@@ -17,19 +17,19 @@ export class PlatosPrincipalesPage implements OnInit {
 
   platos: Plato[] = [
     {
-      imagen: 'assets/img/plato1.jpg',
-      nombre: 'Nombre del plato 1',
-      descripcion: 'Descripción del plato 1'
+      imagen: 'https://imgs.search.brave.com/4C707KTnPRLDUYTvNu2Gp_NaiQb_LvJZ_cfCTnbgA5w/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9wbmcu/cG5ndHJlZS5jb20v/cG5nLWNsaXBhcnQv/MjAyMTExMDUvb3Vy/bWlkL3BuZ3RyZWUt/bmFzaS1nb3Jlbmct/a2FtcHVuZy1zZWFm/b29kLXBuZy1pbWFn/ZV80MDIyNDI1LnBu/Zw',
+      nombre: 'Sopa de Mariscos',
+      descripcion: 'Pescado, camarón, cangrejo, calamar y pulpo en un consomé de vino blanco y ajo.'
     },
     {
-      imagen: 'assets/img/plato2.jpg',
-      nombre: 'Nombre del plato 2',
-      descripcion: 'Descripción del plato 2'
+      imagen: 'https://w7.pngwing.com/pngs/822/38/png-transparent-chicken-parm-thumbnail.png',
+      nombre: 'Pollo a la Parmesana',
+      descripcion: 'Pollo empanizado con salsa marinara casera y queso mozzarella, servido con un acompañamiento de espaghetti marinara y vegetales.'
     },
     {
-      imagen: 'assets/img/plato3.jpg',
-      nombre: 'Nombre del plato 3',
-      descripcion: 'Descripción del plato 3'
+      imagen: 'https://png.pngtree.com/png-vector/20240130/ourmid/pngtree-home-made-spaghetti-bolognese-made-with-meat-and-pasta-isolated-illustration-png-image_11570454.png',
+      nombre: 'Espaguetti de la Casa',
+      descripcion: 'Salsa de tomate casera con ajo aceite de oliva y albahaca fresca con pan de ajo'
     }
   ];
 
